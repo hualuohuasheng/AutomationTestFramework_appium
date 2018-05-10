86530157e6af2e1f30d5585ef22b85284b0b2cdc
liml <liml@sjdd.com.cn>
2018-05-10 18:28:45 +0800

    增加 获取设备信息

---
+++ test_deviceinfo.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+import os
+import re
+import platform
+
+def get_app_info():
+    # get device name
+    readDeviceId = os.popen("adb devices").readlines()
+    deviceId = re.findall(r'^\w*\b', readDeviceId[1])[0]
+
+    # get Android device systerm version
+    deviceVersion = os.popen('adb shell getprop ro.build.version.release').read()
+
+    # get app package name and activity
+    getappInfo = os.popen("adb shell dumpsys window w |findstr \/|findstr name=").read()
+    appInfo = re.findall(r'com.+?tivity', getappInfo)[0].split('/')
+
+    desired_caps = {
+        'platformName': 'Android',
+        'platformVersion': deviceVersion.rstrip('\n'),
+        'deviceName': deviceId,
+        'appPackage': appInfo[0],
+        'appActivity': appInfo[1],
+        'autoAcceptAlerts': 'true',
+        'noReset': 'true'
+    }
+
+    print(desired_caps)
+    return desired_caps
+
+get_app_info()
+
+def get_system_info():
+
+    #  get python version
+    print(platform.python_version())
+
+    print(platform.architecture())
+
+    print(platform.node())
+
+    print(platform.platform())
+
+    print(platform.system())
+
+get_system_info()
+
+command_executor = "http://localhost:4723/wd/hub"
+
+
