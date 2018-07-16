47556829c5b73d2fd57ea9786c5360d8cd9d42c4
liml <liml@sjdd.com.cn>
2018-07-16 17:35:19 +0800

    优化 painting

--- painting.py
+++ painting.py
@@ -1,34 +1,72 @@
+# -*- coding:utf-8 -*-
+
 import matplotlib.pyplot as plt
-import numpy as np
-#x = [1,2,3,4,5,4,3,3,2,55,3,2,2,1]
-#plt.plot(x,color='red')
-#plt.show()
+import math as math
+import re
 
-filename = "f://logs/iroom/pslstreaming_log.txt"
+def var(list, avg):
+    var1 = 0
+    for i in list:
+        var1 += float((i - avg) ** 2 * 1.0)
+    var2 = (math.sqrt(var1 / (len(list) - 1) * 1.0))
+    return var2
 
-delayms,iTargetDelay,curr_videobr,databr,networkbr= [],[],[],[],[]
-with open(filename) as f:
+
+def average(list):
+    avg = sum(list) / float(len(list) * 1.0)
+    return avg
+
+def get_info(filename="f://logs/pslstreaming_log.txt"):
+    delayms, curr_videobr, databr, netbr = [], [], [], []
+
+    f = open(filename)
     lines = f.readlines()
+    for line in lines:
+        line_index = line.find('CheckTransStatus -- delay')
+        if line_index > 0:
+            # print(line)
+            st = re.split(r'\W+',line)
+            delayms.append(int(st[st.index('delay')+1]))
+            databr.append(int(st[st.index('databr')+1]))
+            netbr.append(int(st[st.index('netbr')+1]))
+    info={'delayms': delayms,'databr':databr,'netbr':netbr,'curr_videobr':curr_videobr}
+    return info
 
-for line in lines:
-    # print(line.rstrip())
-    if 'CheckTransStatus' in line:
-       s = line.split(" ")
-       #print(len(s))
-       delayms.append(int(s[13].split("/")[0]))
-       iTargetDelay.append(int(s[29].split(",")[0]))
-       curr_videobr.append(int(s[31].split(",")[0]))
-       databr.append(int(s[33].split(",")[0]))
-       networkbr.append(s[35].split(",")[0])
-       #print(s)
-       #print(s[13].split("/")[0])
-       
-       
-#print(delayms,iTargetDelay,curr_videobr,databr,networkbr)
-#print(iTargetDelay)
 
-plt.plot(delayms,color='red')
-plt.plot(iTargetDelay,color='green', linewidth=5.0, linestyle='-')
-plt.plot(curr_videobr,color='yellow')
-plt.plot(databr,color='black')
-plt.show()
+def paint(info1,info2,name):
+    painting_infos = [f"rc=1 {name}:", max(info1[name]), min(info1[name]),
+           round(average(info1[name]), 2), round(var(info1[name], average(info1[name])), 2)]
+
+    painting_infos1 = [f"rc=2 {name}:", max(info2[name]), min(info2[name]),
+                      round(average(info2[name]), 2), round(var(info2[name], average(info2[name])), 2)]
+
+    #print(info1[name])
+    #设置纵横坐标
+    x = max(len(info1[name]), len(info2[name]))
+    y = max(max(info1[name]), max(info2[name]))
+
+    print(x,y)
+    print(y/20)
+
+    plt.ylim(0, y*1.1)
+    plt.xlim(0, x*1.1)
+
+    #
+    plt.text(45, y*1.1+y/20, painting_infos, fontsize=10, color='black')
+    plt.text(45, y*1.1+y/20*2, painting_infos1, fontsize=10, color='black')
+
+    #示例
+    plt.plot(info1[name], color='red', linewidth=0.3, linestyle="-", label=f"rc=1 {name}")
+    plt.plot(info2[name], color='green', linewidth=0.3, linestyle="-", label=f"rc=2 {name}")
+    plt.legend(loc='upper left', frameon=False)
+
+    plt.savefig(f'f://logs/painting_{name}.png')
+    plt.close()
+
+file_info=get_info()
+file_info1=get_info("f://logs/pslstreaming_log1.txt")
+
+
+paint(file_info,file_info1,'delayms')
+paint(file_info,file_info1,'databr')
+paint(file_info,file_info1,'netbr')
